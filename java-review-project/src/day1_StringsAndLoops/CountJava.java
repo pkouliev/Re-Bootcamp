a98bb7e375ccd41d58303b8803a6a1480cae77ef
pkouliev <pkouliev@gmail.com>
2020-05-28 10:10:55 -0400

    CountJava class added

---
+++ java-review-project/src/day1_StringsAndLoops/CountJava.java
@@ -0,0 +1,35 @@
+package day1_StringsAndLoops;
+
+public class CountJava {
+
+    public static void main(String[] args) {
+
+
+        String str = "java is fun. java is the best";
+        int java = 0;
+
+        System.out.println("First Approach");
+
+        for (int i = 0; i < str.length() - 3; i++) {
+
+            if (str.substring(i, i + 4).equalsIgnoreCase("java")) java++;
+        }
+
+        System.out.println("java was found " + java + " times on first approach\n");
+
+        System.out.println("Second Approach");
+
+        int java2 = 0;
+
+        str = str.toLowerCase();
+
+        while (str.contains("java")) {
+
+            java2++;
+
+            str = str.replaceFirst("java", "");
+        }
+
+        System.out.println("java was found " + java2 + " times on second approach");
+    }
+}
