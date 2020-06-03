450eb8032bc51b61d2ca8dcf261bbe87543b5cf5
pkouliev <pkouliev@gmail.com>
2020-06-03 00:38:59 -0400

    Example.java created

---
+++ java-review-project/src/day2_ClassesAndMethods/Example.java
@@ -0,0 +1,21 @@
+package day2_ClassesAndMethods;
+
+public class Example {
+
+    public static void main(String[] args) {
+        new Example(7, 4);
+    }
+
+    public Example() {
+        System.out.println("no param");
+    }
+
+    public Example(int a) {
+        System.out.println(a);
+    }
+
+    public Example(int a, int b) {
+        this(a);
+        System.out.println(b);
+    }
+}
