f3f55c32f5b60a82be12607260e23ac432f47290
pkouliev <pkouliev@gmail.com>
2020-06-04 14:56:54 -0400

    Dog.java

---
+++ java-review-project/src/day3_OOP/Dog.java
@@ -0,0 +1,15 @@
+package day3_OOP;
+
+public class Dog extends Animal {
+
+    // Dog is a Animal
+    // Dog has a name
+
+    public Dog() {
+
+    }
+
+    public Dog(int i) {
+        super(i);
+    }
+}
