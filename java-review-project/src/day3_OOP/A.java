44b6118915e9d44d57b45bb738aa43cf0bc3a43e
pkouliev <pkouliev@gmail.com>
2020-06-06 13:41:23 -0400

    A.java

---
+++ java-review-project/src/day3_OOP/A.java
@@ -0,0 +1,16 @@
+package day3_OOP;
+
+public class A {
+
+    int num = 10;
+
+    public final void method2() {
+
+    }
+
+
+    public Object get() {
+        System.out.println("A");
+        return new Animal();
+    }
+}
