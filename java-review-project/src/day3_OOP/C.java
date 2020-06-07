7aa3b9f8d001100049149eaa5a6db3d364c56dfa
pkouliev <pkouliev@gmail.com>
2020-06-07 12:42:28 -0400

    C.java

---
+++ java-review-project/src/day3_OOP/C.java
@@ -0,0 +1,18 @@
+package day3_OOP;
+
+public class C {
+
+    public static void main(String[] args) {
+
+
+        B.method1();
+
+        B b = new B();
+        b.get();
+
+
+        System.out.println(b.num);
+
+    }
+
+}
