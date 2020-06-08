6ce282f74acdedd58edbeb1d1501c62da0a7c63c
pkouliev <pkouliev@gmail.com>
2020-06-08 14:05:50 -0400

    ClassOne

---
+++ java-review-project/src/day4_OOP/ClassOne.java
@@ -0,0 +1,32 @@
+package day4_OOP;
+
+public class ClassOne implements Teachable, Movable {
+
+    // int size = 0;
+
+    @Override
+    public void doHomework() {
+
+    }
+
+    @Override
+    public void learn() {
+
+    }
+
+    @Override
+    public void teachMath() {
+        System.out.println(Teachable.size);
+
+    }
+
+    @Override
+    public void moveTo() {
+
+    }
+
+    @Override
+    public void rotate() {
+
+    }
+}
