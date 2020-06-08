4e54978301ee71c14ada2db3362a596521f2c85a
pkouliev <pkouliev@gmail.com>
2020-06-08 14:09:02 -0400

    TV.java

---
+++ java-review-project/src/day4_OOP/TV.java
@@ -0,0 +1,32 @@
+package day4_OOP;
+
+public class TV implements Movable, Upgradeable {
+
+    @Override
+    public void moveTo() {
+
+    }
+
+    public void rotate() {
+        //
+    }
+
+    public void increaseSize() {
+        //
+    }
+
+    @Override
+    public void doHomework() {
+
+    }
+
+    @Override
+    public void learn() {
+
+    }
+
+    @Override
+    public void teachMath() {
+
+    }
+}
