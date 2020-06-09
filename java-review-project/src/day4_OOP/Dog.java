0c74c87dc214a8d939fae1fad7a3f5e7318caa9e
pkouliev <pkouliev@gmail.com>
2020-06-08 21:48:18 -0400

    Dog.java

---
+++ java-review-project/src/day4_OOP/Dog.java
@@ -0,0 +1,10 @@
+package day4_OOP;
+
+public class Dog extends Animal implements Tameable {
+
+    Dog dog = new Dog(); //  dog reference to dog (itself) object
+
+    Animal dog1 = new Dog(); // Animal reference as super class to dog object
+
+    Tameable dog3 = new Dog(); // Tameable reference as interface t0 dog object implementation
+}
