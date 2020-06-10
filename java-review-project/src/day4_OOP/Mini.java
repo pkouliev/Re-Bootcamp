992818d793be8b848cf6f99c8cb4261028c03524
pkouliev <pkouliev@gmail.com>
2020-06-10 14:13:42 -0400

    Updates

--- java-review-project/src/day4_OOP/Mini.java
+++ java-review-project/src/day4_OOP/Mini.java
@@ -3,17 +3,25 @@ package day4_OOP;
 public class Mini extends Car {
 
     static void m1() {
+
         System.out.println("c");
     }
 
     static void m2() {
+
         System.out.println("d");
     }
 
     public static void main(String[] args) {
 
         Car c = new Mini();
-        c.m1();
-        c.m2();
+        c.m1(); // a
+        c.m2(); // b
+
+        Mini.m1(); // c
+        Mini.m2(); // d
+        ((Mini) c).m1(); // c
+        ((Mini) c).m2(); // d
+
     }
 }
