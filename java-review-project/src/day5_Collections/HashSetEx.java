99dcbbb2be88c50e6d1d7b73260864db5972c53f
pkouliev <pkouliev@gmail.com>
2020-06-11 11:55:26 -0400

    HashSetEx

---
+++ java-review-project/src/day5_Collections/HashSetEx.java
@@ -0,0 +1,23 @@
+package day5_Collections;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetEx {
+
+    public static void main(String[] args) {
+
+        Set<String> set = new HashSet<>();
+        set.add("z");
+        set.add("y");
+        set.add(null);
+        set.add("z");
+        set.add("80");
+        set.add("/");
+        set.add(null);
+        set.add("z");
+        set.add("a");
+
+        System.out.println(set);
+    }
+}
