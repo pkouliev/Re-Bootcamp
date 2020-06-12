503d4f3656243032f32f80c68bc2a1b9523354fb
pkouliev <pkouliev@gmail.com>
2020-06-12 14:27:48 -0400

    ExceptionEx

---
+++ java-review-project/src/day5_Collections/ExceptionEx.java
@@ -0,0 +1,21 @@
+package day5_Collections;
+
+public class ExceptionEx {
+
+    public static void main(String[] args) {
+
+        try {
+            String s = "java";
+
+            System.out.println(s.substring(1));
+            System.out.println(4 / 4);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("finally block");
+        }
+
+    }
+}
